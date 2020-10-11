86b17de41dd39e2dee3604ecc428c7232b5d6528
Francisco Santamaria <appmanwebs@gmail.com>
2020-10-10 22:33:50 -0500

    Updates

---
+++ precache-manifest.3b67d1fc7b60ccdfc0c1e892d41980f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dd3f26f0075012eea7585cede2f3b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "547464c1f3b856c44be1",
+    "url": "/monsters-rolodex/static/css/main.ad575167.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547464c1f3b856c44be1",
+    "url": "/monsters-rolodex/static/js/main.df889d67.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
